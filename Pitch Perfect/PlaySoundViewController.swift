5fabfc3425ace146e15e7cb96ee70c191a546426
xiaozhenliu <zjulxz@gmail.com>
2015-03-28 17:32:54 +0800

    Add playSlowAudio Button
    Add playSlowAudio Button in the second viewController

---
+++ Pitch Perfect/PlaySoundViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PlaySoundViewController.swift
+//  Pitch Perfect
+//
+//  Created by AXZ on 3/28/15.
+//  Copyright (c) 2015 AXZ. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundViewController: UIViewController {
+   
+    @IBAction func playSlowAudio(sender: UIButton) {
+    }
+}
